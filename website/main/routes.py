c46048926ece54990eb665b62062ac43ae8b412d
AdamWang00 <AdamWang00@users.noreply.github.com>
2020-07-13 18:56:31 -0400

    cleanup

--- website/main/routes.py
+++ website/main/routes.py
@@ -8,10 +8,3 @@ main = Blueprint('main', __name__)
 def home():
     return render_template('home.html')
 
-@main.route("/text")
-def text():
-    return render_template('junk_mail.html')
-
-@main.route("/image")
-def image():
-    return render_template('image_rec.html')
