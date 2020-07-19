cbdc48912ae17ae1e9b72f56bb735f85a298f47a
Manoj Niverthi <manojniverthi@gmail.com>
2020-07-19 06:03:43 -0400

    changed spacing and name

--- website/static/script.js
+++ website/static/script.js
@@ -1,10 +1,10 @@
 setInterval(() => {
-    var c = document.getElementById("myCanvas");
+    var c = document.getElementById("easel");
     var ctx = c.getContext("2d");
-    ctx.strokeStyle = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    ctx.strokeStyle = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     ctx.beginPath();
-    ctx.moveTo(Math.random()*800, Math.random()*800);
-    ctx.lineTo(Math.random()*800, Math.random()*800);
+    ctx.moveTo(Math.random() * 800, Math.random() * 800);
+    ctx.lineTo(Math.random() * 800, Math.random() * 800);
     ctx.stroke();
     ctx.endPath();
 }, 1000);
