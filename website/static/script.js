ca4e44bab4fa6606f4cc4a8742954a3d4aeb86d5
AdamWang00 <AdamWang00@users.noreply.github.com>
2020-07-19 16:53:06 -0400

    Update script.js

--- website/static/script.js
+++ website/static/script.js
@@ -1,10 +1,18 @@
+const c = document.getElementById("easel");
+const ctx = c.getContext("2d");
+
 setInterval(() => {
-    var c = document.getElementById("easel");
-    var ctx = c.getContext("2d");
     ctx.strokeStyle = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+    const xstart = Math.random() * 800;
+    const ystart = Math.random() * 800;
+    const xend = Math.random() * 800;
+    const yend = Math.random() * 800;
+    ctx.lineWidth = Math.sqrt(Math.pow(Math.abs(xstart - xend), 2) + Math.pow(Math.abs(ystart - yend), 2)) / 40;
+    
     ctx.beginPath();
-    ctx.moveTo(Math.random() * 800, Math.random() * 800);
-    ctx.lineTo(Math.random() * 800, Math.random() * 800);
+    ctx.moveTo(xstart, ystart);
+    ctx.lineTo(xend, yend);
     ctx.stroke();
     ctx.endPath();
 }, 1000);
