1b4482ade82415c724ad99fef1de879dc085354d
AdamWang00 <AdamWang00@users.noreply.github.com>
2020-07-19 19:36:28 -0400

    naice

--- website/static/script.js
+++ website/static/script.js
@@ -1,16 +1,19 @@
 const c = document.getElementById("easel");
 const ctx = c.getContext("2d");
+const cW =  c.width
+const cH = c.height
+const ext = 50
 
-var xstart = Math.random() * 800;
-var ystart = Math.random() * 800;
+var xstart = Math.random() * (cW + 2 * ext) - ext;
+var ystart = Math.random() * (cH + 2 * ext) - ext;
 
-ctx.globalAlpha = 0.1;
+ctx.globalAlpha = 0.08;
 
 setInterval(() => {
     ctx.strokeStyle = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
-    const xend = Math.random() * 800;
-    const yend = Math.random() * 800;
+    const xend = Math.random() * (cW + 2 * ext) - ext;
+    const yend = Math.random() * (cW + 2 * ext) - ext;
     ctx.lineWidth = Math.sqrt(Math.pow(Math.abs(xstart - xend), 2) + Math.pow(Math.abs(ystart - yend), 2)) / 40;
     
     ctx.beginPath();
