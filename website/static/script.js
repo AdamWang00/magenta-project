c44b7e3f4baba774f77764bf988c615ea17c0996
Manoj Niverthi <manojniverthi@gmail.com>
2020-07-19 06:01:46 -0400

    added line pathing

--- website/static/script.js
+++ website/static/script.js
@@ -2,7 +2,9 @@ setInterval(() => {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.strokeStyle = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    ctx.beginPath();
     ctx.moveTo(Math.random()*800, Math.random()*800);
     ctx.lineTo(Math.random()*800, Math.random()*800);
     ctx.stroke();
+    ctx.endPath();
 }, 1000);
