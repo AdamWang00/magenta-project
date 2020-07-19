1cccac670a5ee3a88cfb3dc002cd7ab2b9f1a82e
AdamWang00 <AdamWang00@users.noreply.github.com>
2020-07-19 16:24:32 -0400

    Update script.js

--- website/static/script.js
+++ website/static/script.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+setInterval(() => {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.strokeStyle = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    ctx.moveTo(Math.random()*800, Math.random()*800);
+    ctx.lineTo(Math.random()*800, Math.random()*800);
+    ctx.stroke();
+}, 1000);
